837f71e46bdadff82fe65d0c2cc04778cc50750a
Payam3006 <payam3006@gmail.com>
2024-05-08 16:04:45 +0330

    better reset

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const q = console.log;
 
+const reset = () => {
+  document.getElementById("remained").style = `height: 100%;`;
+  document.getElementById("remNum").innerText = `2L`;
+
+  document.getElementById("filled").style = `height:0%;`;
+  document.getElementById("filled").innerText = `0%`;
+  for (let i = 1; i <= 8; i++) {
+    document
+      .getElementsByClassName("glass")
+      [i - 1].classList.remove("fillGlass");
+  }
+};
+
 const fill = (numberOfGlasses) => {
   let unfilledLitter = (8 - numberOfGlasses) * 12.5;
   document.getElementById("remained").style = `height: ${unfilledLitter}%;`;
